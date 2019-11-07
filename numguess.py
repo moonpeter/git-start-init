9ebd8eb068cb5be0eeea0e8a6f38bae0d334664b
moonpeter <m88nkhv@naver.com>
2019-11-07 14:39:37 +0900

    feat: compare guess and answer >TODO chance to challenge several times"

--- numguess.py
+++ numguess.py
@@ -4,4 +4,7 @@ from random import randint
 answer = randint(1,100+1)
 print(answer)
 guess = int(input('Hi, guess the num(1, 100+1)> '))
-
+if guess == answer:
+    print("True")
+else:
+    print("False")
